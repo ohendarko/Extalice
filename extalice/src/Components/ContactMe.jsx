15ba6776294dd2213f865144baed1bbd52ca2d60
ohendarko <ohendarko@gmail.com>
2025-03-22 22:19:27 +0000

    including whatsapp link

--- extalice/src/Components/ContactMe.jsx
+++ extalice/src/Components/ContactMe.jsx
@@ -32,6 +32,15 @@ const ContactMe = () => {
               </svg>
             </a>
           </div>
+          
+          <div className="youtube social-icon">
+            <a href="https://youtube.com/@e_star_lice?si=ZxTbgZX9hfSSc1yA" className="yt-link">
+              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="48" height="48" viewBox="0 0 24 24">
+                <path d="M21.582,6.186c-0.23-0.86-0.908-1.538-1.768-1.768C18.254,4,12,4,12,4S5.746,4,4.186,4.418 c-0.86,0.23-1.538,0.908-1.768,1.768C2,7.746,2,12,2,12s0,4.254,0.418,5.814c0.23,0.86,0.908,1.538,1.768,1.768 C5.746,20,12,20,12,20s6.254,0,7.814-0.418c0.861-0.23,1.538-0.908,1.768-1.768C22,16.254,22,12,22,12S22,7.746,21.582,6.186z M10,15.464V8.536L16,12L10,15.464z"></path>
+              </svg>
+            </a>
+          </div>
+
         </div>
         <h1 className="sign-latest">Sign Up For My Latest</h1>
         <SignForm />
