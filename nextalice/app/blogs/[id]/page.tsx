a717f8bc41a506d2202cb166ef3843b187fede56
ohendarko <ohendarko@gmail.com>
2025-05-27 18:18:37 +0000

    added back button, used React.use() to handle params. Eslint error handled

--- nextalice/app/blogs/[id]/page.tsx
+++ nextalice/app/blogs/[id]/page.tsx
@@ -1,17 +1,20 @@
-// app/blogs/[id]/page.tsx
+'use client'
+import { use } from 'react';
 import styles from '@/app/styles/blog.module.css';
 import blogPost from '@/assets/sample-blog'; // Adjust if outside src
 import NotFoundPage from '@/components/NotFoundPage';
 import ContactMe from '@/components/ContactMe';
+import { useRouter } from 'next/navigation';
 
 type BlogPageProps = {
-  params: { id: string };
+  params: Promise<{ id: string }>; // notice params is a Promise now
 };
 
-const BlogPage = async ({ params }: BlogPageProps) => {
+const BlogPage = ({ params }: BlogPageProps) => {
 
-  const resolvedParams = await params;   // await here
-  const blog = blogPost.find((b) => b.id === resolvedParams.id);
+  const router = useRouter();
+  const { id } = use(params);
+  const blog = blogPost.find((b) => b.id === id);
 
   // const blog = blogPost.find((b) => b.id === params.id);
   if (!blog) {
@@ -21,6 +24,7 @@ const BlogPage = async ({ params }: BlogPageProps) => {
   return (
     <div className={styles.blogPage}>
       <div className={styles.blogContents}>
+        <button className='w-[100px] flex gap-2' onClick={() => router.back()}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M3 19V5"/><path d="m13 6-6 6 6 6"/><path d="M7 12h14"/></svg> Back</button>
         <h1 className={styles.blogTitle}>{blog.title}</h1>
         <div className={styles.blogContent} dangerouslySetInnerHTML={{ __html: blog?.content}} />
         
