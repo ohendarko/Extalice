0cc185c157897d4cb07eb8354378c96fd5ed4ff9
ohendarko <ohendarko@gmail.com>
2025-05-27 17:48:16 +0000

    no ununsed vars and other eslint warnings handled

--- nextalice/app/layout.tsx
+++ nextalice/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins, Roboto, Roboto_Mono } from 'next/font/google';
 import "./globals.css";
 import Header from "@/components/Header";
